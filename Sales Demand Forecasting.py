63f27d782bba296ad9cd186553065f27e863f370
pg2808419@-hi <pg2808419@gmail.com>
2026-03-11 16:58:31 +0530

    Updated sales forecasting code

--- Sales Demand Forecasting.py
+++ Sales Demand Forecasting.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
 
 # 2. Load Dataset
 import os
@@ -48,7 +48,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # 8. Train Model
-model = LinearRegression()
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 print("\nModel Training Completed")
 
